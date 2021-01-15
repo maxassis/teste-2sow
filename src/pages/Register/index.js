9970d621d5e553f32e96ca7f7b1824af6edc9494
maxassis <max.assis@ymail.com>
2021-01-15 13:32:12 -0300

    implementando a busca por cep

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,34 +1,50 @@
 import React, { useState } from "react";
 import * as yup from "yup";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as S from "./styles";
 import Header from "../../components/header/index";
 import axios from "axios";
 
 export default function Register() {
-  const [inpstate, setInpState] = useState("");
+  const [toggle, setToggle] = useState(false);
 
   const schema = yup.object().shape({
+    cpf: yup.string().required("Cpf obrigatorio"),
+    cep: yup.string().required("Cep e obrigatorio"),
     nome: yup.string().required("Nome e obrigatorio"),
+    email: yup.string().email().required("Email e obrigatorio"),
+    //numero: yup.string().required("numero e obrigatorio"),
   });
 
-  const { register, handleSubmit, errors } = useForm({
+  const { register, handleSubmit, errors, control } = useForm({
     resolver: yupResolver(schema),
   });
 
   function onSubmit(data) {
     console.log(data);
-  }
+    const cep = data.cep.replace(/-/g, "").replace(/\./g, "");
 
-  /*  const cepFetch = async () => {
-    const { data } = await axios.get(`https://viacep.com.br/ws/cep/json/`);
-    setInpState(data.localidade);
-    document.getElementById("rua").value = data.logradouro;
-    document.getElementById("bairro").value = data.bairro;
-    document.getElementById("cidade").value = data.localidade;
-    return data;
-  }; */
+    axios
+      .get(`https://viacep.com.br/ws/${cep}/json/`)
+      .then((response) => {
+        console.log(response.request.status);
+        if (
+          response.request.status === 200 &&
+          response.data.erro === undefined
+        ) {
+          document.getElementById("cidade").value = response.data.localidade;
+          document.getElementById("bairro").value = response.data.bairro;
+          document.getElementById("rua").value = response.data.logradouro;
+        } else if (response.data.erro === true) {
+          alert("deu ruim");
+        }
+      })
+      .catch((error) => {
+        alert("deu erro");
+        console.log(error.response);
+      });
+  }
 
   return (
     <>
@@ -37,11 +53,65 @@ export default function Register() {
         <h1>Cadastro</h1>
       </S.WrapperTitle>
       <S.WrapperForm>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input name="nome" ref={register()}></input>
-          {errors.nome?.message}
-          <button>teste</button>
-        </form>
+        <S.Frm onSubmit={handleSubmit(onSubmit)}>
+          <S.Input name="nome" ref={register()} placeholder="Nome" />
+          {errors.nome && <S.Span>O nome e obrigatorio</S.Span>}
+          <Controller
+            as={S.InputMsk}
+            control={control}
+            mask="999.999.999-99"
+            name="cpf"
+            placeholder="Cpf"
+            defaultValue=""
+          />
+          {errors.cpf && <S.Span>O cpf e obrigatorio</S.Span>}
+          <S.Input name="email" ref={register()} placeholder="Email" />
+          {errors.email && <S.Span>Digite um email valido</S.Span>}
+          <div>
+            <Controller
+              as={S.InptCep}
+              control={control}
+              mask="99999-999"
+              name="cep"
+              placeholder="Cep"
+              defaultValue=""
+            />
+            <S.BtnCep type="button">Buscar Cep</S.BtnCep>
+            <br /> {errors.cep && <S.Span>O cep e obrigatorio</S.Span>}
+          </div>
+          <S.Input
+            name="numero"
+            type="number"
+            ref={register()}
+            placeholder="Numero"
+          />
+          {errors.numero && <S.Span>O numero e obrigatorio</S.Span>}
+          <S.Input
+            name="rua"
+            id="rua"
+            ref={register()}
+            placeholder="Rua"
+            readOnly="true"
+          />
+          {errors.rua && <S.Span>A rua e obrigatorio</S.Span>}
+          <S.Input
+            name="bairro"
+            id="bairro"
+            ref={register()}
+            placeholder="Bairro"
+            readOnly="true"
+          />
+          {errors.bairro && <S.Span>O bairro e obrigatorio</S.Span>}
+          <S.Input
+            name="cidade"
+            id="cidade"
+            ref={register()}
+            placeholder="Cidade"
+            readOnly="true"
+          />
+          {errors.cidade && <S.Span>A cidade e obrigatorio</S.Span>}
+          <S.Btn>Enviar</S.Btn>
+        </S.Frm>
       </S.WrapperForm>
     </>
   );
--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
-import InputMask from "../../components/inputWithMask/input";
-import { Form } from "@unform/web";
+import InputMask from "react-input-mask";
 
 export const WrapperTitle = styled.div`
   width: 100%;
@@ -14,22 +13,23 @@ export const WrapperTitle = styled.div`
 
 export const WrapperForm = styled.div`
   width: 100%;
-  height: 700px;
+  height: 630px;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-export const Frm = styled(Form)`
+export const Frm = styled.form`
   width: 35%;
   display: grid;
-  grid-gap: 1.8rem;
+  grid-gap: 1rem;
   background-color: white;
   padding: 2rem;
   border-radius: 8px;
+  grid-template-rows: auto;
 `;
 
-export const MaskInput = styled(InputMask)`
+export const Input = styled.input`
   width: 100%;
   height: 43px;
   border-radius: 8px;
@@ -52,4 +52,47 @@ export const Btn = styled.button`
   color: white;
   font-size: 16px;
   font-weight: 600;
+`;
+
+export const InputMsk = styled(InputMask)`
+  width: 100%;
+  height: 43px;
+  border-radius: 8px;
+  outline: none;
+  border: 1.5px solid;
+  border-color: #dfe0df;
+  background-color: #f5f4f4;
+  padding-left: 18px;
+  box-sizing: border-box;
+`;
+
+export const Span = styled.span`
+  font-size: 12px;
+`;
+
+export const InptCep = styled(InputMask)`
+  width: 66.5%;
+  height: 43px;
+  border-radius: 8px;
+  outline: none;
+  border: 1.5px solid;
+  border-color: #dfe0df;
+  background-color: #f5f4f4;
+  padding-left: 18px;
+  box-sizing: border-box;
+`;
+
+export const BtnCep = styled.button`
+  width: 30%;
+  height: 43px;
+  border-radius: 10px;
+  outline: none;
+  background-color: #af0069;
+  border: none;
+  font-family: "Open Sans", sans-serif;
+  color: white;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 1;
+  margin-left: 15px;
 `;
